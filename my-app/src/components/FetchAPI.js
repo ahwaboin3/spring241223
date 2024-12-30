15937f6424d0178dc02788f6faa81dd1ce53acc9
devgiants75 <56944017+ahwaboin3@users.noreply.github.com>
2024-12-30 11:49:15 +0900

    47 n74 n7n4

---
+++ my-app/src/components/FetchAPI.js
@@ -0,0 +1,11 @@
+//FetchAPI.js
+
+function FetchAPI(){
+    //fetch API 이용
+    //fetch API를 이용하면 웹 요청을 쉽게 만들 수 있다.
+    //fetch API는 기존의 XMLHttpRequet(AJAX)와 비슷한 개념이지만 프로미스를 지원하므로 더 직관적으로 이용할 수 있다.
+
+    return <></>
+}
+
+export default FetchAPI
